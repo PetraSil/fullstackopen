1e9f26df23f0ee4f6a7e95ef63ac258ba92c8f41
Petra <petra.silavuori@gmail.com>
2019-10-05 15:10:37 +0300

    1.6 done.

--- 1_d/src/App.js
+++ 1_d/src/App.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
 
-const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+const Button = (props) => {
+  return (
+    <button onClick={props.clickHandler}>{props.name}</button>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const IncrementGood = () => {
+    setGood(good + 1);
+  };
+
+  const IncrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const IncrementBad = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
-          left
-        </button>
-        <button onClick={() => setRight(right + 1)}>
-          right
-        </button>
-        {right}
-      </div>
+      <h2>RATE UNICAFE SERVICE:</h2>
+      <Button name="Good" clickHandler={() => IncrementGood()} />
+      <Button name="Neutral" clickHandler={() => IncrementNeutral()} />
+      <Button name="Bad" clickHandler={() => IncrementBad()} />
+      <h2>INFORMATION:</h2>
+      <h3>Good reviews: {good}</h3>
+      <h3>Neutral reviews: {neutral}</h3>
+      <h3>Bad reviews: {bad}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default App;
