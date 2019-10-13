6c1c1eb1f9fb89a57333703539c239e1d4f7eb7a
Petra <petra.silavuori@gmail.com>
2019-10-13 12:32:20 +0300

    2.2 - 2.3

--- 2_a/src/components/Course.js
+++ 2_a/src/components/Course.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Content from './Content';
+import Total from './Total';
 
 const Course = ({ course }) => {
     return (
         <div>
             <Header title={course.name} />
             <Content parts={course.parts} name={course.parts.name} exercises={course.parts.exercises} />
+            <Total sum={course.parts}/>
         </div>
     );
 };
--- 2_a/src/components/Total.js
+++ 2_a/src/components/Total.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Total = () => {
+const Total = ({ sum }) => {
+
+    const total = sum.map( item => item.exercises).reduce((accumulating, current) => accumulating + current )
+
     return (
         <div>
-
+            <strong>Number of exercises: {total}</strong>
         </div>
     );
 };
