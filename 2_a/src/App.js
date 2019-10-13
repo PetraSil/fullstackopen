9ec7b09b3e785544ff3965364fd90fbc40832c83
Petra <petra.silavuori@gmail.com>
2019-10-13 12:46:08 +0300

    2.5

--- 2_a/src/App.js
+++ 2_a/src/App.js
@@ -2,35 +2,53 @@ import React from 'react';
 import Course from './components/Course';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  
 
   return (
     <div>
-      <Course course={course} parts={course.parts}/>
+      {courses.map( item => <Course course={item} parts={courses.parts} /> )}
     </div>
   )
 }
--- 2_a/src/components/Content.js
+++ 2_a/src/components/Content.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Part from './Part';
 
 const Content = (props) => {
-    
-    const courseList = props.parts.map( item => (<Part key={item.id} name={item.name} exercises={item.exercises}/>))
+
+    const courseList = props.parts.map( item => (<Part key={item.id} name={item.name} exercises={item.exercises} /> ))
 
     return (
         <div>
